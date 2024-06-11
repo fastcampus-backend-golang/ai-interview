5638ef450352992c182eb96732226f85989be94b
Indra Made <indra.made@frankieone.com>
2024-06-11 16:31:27 +0700

    added ai asset getter

---
+++ ai/assets.go
@@ -0,0 +1,64 @@
+package ai
+
+import (
+	"encoding/base64"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+func GetInitialAudio() (string, error) {
+	basedir, err := os.Getwd()
+	if err != nil {
+		log.Printf("error: %v\n", err)
+		return "", err
+	}
+
+	assetPath := filepath.Join(basedir, "ai", "assets", "initial.mp3")
+
+	audio, err := os.ReadFile(assetPath)
+	if err != nil {
+		log.Printf("error: %v\n", err)
+		return "", err
+	}
+
+	encoded := base64.StdEncoding.EncodeToString(audio)
+
+	return encoded, nil
+}
+
+func GetInitialText() (string, error) {
+	basedir, err := os.Getwd()
+	if err != nil {
+		log.Printf("error: %v\n", err)
+		return "", err
+	}
+
+	assetPath := filepath.Join(basedir, "ai", "assets", "initial.txt")
+
+	text, err := os.ReadFile(assetPath)
+	if err != nil {
+		log.Printf("error: %v\n", err)
+		return "", err
+	}
+
+	return string(text), nil
+}
+
+func GetSystemPrompt() (string, error) {
+	basedir, err := os.Getwd()
+	if err != nil {
+		log.Printf("error: %v\n", err)
+		return "", err
+	}
+
+	assetPath := filepath.Join(basedir, "ai", "assets", "system.txt")
+
+	text, err := os.ReadFile(assetPath)
+	if err != nil {
+		log.Printf("error: %v\n", err)
+		return "", err
+	}
+
+	return string(text), nil
+}
